375ed741d077271910649d390e5c337bcafa3f24
Willians-S-S <09willians@gmail.com>
2025-12-15 22:36:59 -0300

    feat: adicionar validações no AccountDTO
    - Valida nome com tamanho mínimo (3) e regex para permitir apenas letras e espaços
    - Adiciona validação de formato de e-mail com @Email
    - Valida senha com tamanho mínimo (8) e mensagem personalizada

--- src/main/java/br/com/wss/barbersync/dtos/AccountDTO.java
+++ src/main/java/br/com/wss/barbersync/dtos/AccountDTO.java
@@ -5,6 +5,9 @@ import br.com.wss.base.BaseDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,6 +19,11 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = false)
 public class AccountDTO extends BaseDTO<String> {
 
+    @Size(min=3, message = "Nome deve conter no mínimo 3 letras")
+    @Pattern(
+            regexp = "^[\\p{L} ]+$",
+            message = "Nome deve conter apenas letras e espaços"
+    )
     private String name;
 
     private String taxNumber;
@@ -26,8 +34,10 @@ public class AccountDTO extends BaseDTO<String> {
 
     private String phone;
 
+    @Email
     private String email;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Size(min=8, message = "Senha deve conter no mínimo 8 caracteres")
     private String password;
 }
