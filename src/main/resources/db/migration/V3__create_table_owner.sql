7e80e1c9441d8753bdcd575e2292a4dc4a919da7
Willians-S-S <09willians@gmail.com>
2026-01-19 21:39:29 -0300

    db: criar tabela owner e estabelecer relacionamento com account

---
+++ src/main/resources/db/migration/V3__create_table_owner.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS owner (
+    uid varchar(255) NOT NULL,
+    created_at timestamp(6) NULL,
+    created_by_name varchar(255) NULL,
+    created_by_uid varchar(255) NULL,
+    deleted bool NULL,
+    deleted_at timestamp(6) NULL,
+    deleted_by_name varchar(255) NULL,
+    deleted_by_uid varchar(255) NULL,
+    updated_at timestamp(6) NULL,
+    updated_by_name varchar(255) NULL,
+    updated_by_uid varchar(255) NULL,
+    account_id varchar(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(uid)
+);
