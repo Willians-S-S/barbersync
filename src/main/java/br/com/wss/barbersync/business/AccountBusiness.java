a80873081ac54f8ded320e2d345fb0aaa64803cc
Willians-S-S <09willians@gmail.com>
2026-02-03 21:36:38 -0300

    refactor: renomear Role.java para RoleAccountEnum.java para maior clareza e precisão do tipo

--- src/main/java/br/com/wss/barbersync/business/AccountBusiness.java
+++ src/main/java/br/com/wss/barbersync/business/AccountBusiness.java
@@ -1,7 +1,7 @@
 package br.com.wss.barbersync.business;
 
 import br.com.wss.barbersync.entities.Account;
-import br.com.wss.barbersync.enums.Role;
+import br.com.wss.barbersync.enums.RoleAccountEnum;
 import br.com.wss.base.BaseBusiness;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +17,8 @@ public interface AccountBusiness extends BaseBusiness<Account, String> {
     List<Account> findAll();
 
     Page<Account> findByParams(final String uid, final String name, final String taxNumber, final String email, final String phone, final String createdByName, final String updatedByName,
-                                         final Role role, final Boolean active, final LocalDateTime createdStartAt, final LocalDateTime createdEndAt,
-                                         final Pageable pageable);
+                               final RoleAccountEnum roleAccountEnum, final Boolean active, final LocalDateTime createdStartAt, final LocalDateTime createdEndAt,
+                               final Pageable pageable);
 
     Account getCurrentAccount();
 
